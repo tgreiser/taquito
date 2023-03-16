bf5524b2f52fa9ba38b68f9b2422e76bb25ab622
Zainen Suzuki <zainensuzuki@Zainens-MacBook-Pro.local>
2023-03-16 14:59:28 -0700

    fix: fix test to handle possible reveal

--- integration-tests/util-toDryRun-toForge.spec.ts
+++ integration-tests/util-toDryRun-toForge.spec.ts
@@ -5,39 +5,44 @@ import { CONFIGS } from "./config";
 
 
 CONFIGS().forEach(({ lib, setup, createAddress }) => {
-  const Tezos = lib
-  const forger = new LocalForger()
+  const Tezos = lib;
+  const forger = new LocalForger();
 
   describe(`Test taquito common util functions in context`, () => {
     beforeEach(async (done) => {
-      await setup()
-      done()
+      await setup();
+      done();
     })
 
     test('Verify that toDryRun returns executable for preapplyOperations', async (done) => {
       const reciever = await createAddress();
       const pkh = await reciever.signer.publicKeyHash();
-      const preparedTransfer = await Tezos.prepare.transaction({ amount: 1, to: pkh })
+      const preparedTransfer = await Tezos.prepare.transaction({ amount: 1, to: pkh });
 
-      const forged = await forger.forge(toForge(preparedTransfer))
+      const forged = await forger.forge(toForge(preparedTransfer));
       const signOp = await Tezos.signer.sign(forged, new Uint8Array([3]));
 
-      const preapply = await Tezos.rpc.preapplyOperations(toDryRun(preparedTransfer, signOp))
+      const preapply = await Tezos.rpc.preapplyOperations(toDryRun(preparedTransfer, signOp));
 
-      expect(preapply[0].contents[0].kind).toEqual('transaction')
+      if (preapply[0].contents[0].kind === 'reveal') {
+        expect(preapply[0].contents[0].kind).toEqual('reveal');
+        expect(preapply[0].contents[1].kind).toEqual('transaction');
+      } else {
+        expect(preapply[0].contents[0].kind).toEqual('transaction');
+      }
 
-      done()
+      done();
     });
 
     it('Verify that toForge is exectuable for both local forger and rpc.forgeOperations', async (done) => {
       const reciever = await createAddress();
       const pkh = await reciever.signer.publicKeyHash();
-      const preparedTransfer = await Tezos.prepare.transaction({ amount: 1, to: pkh })
+      const preparedTransfer = await Tezos.prepare.transaction({ amount: 1, to: pkh });
 
-      const forged = await forger.forge(toForge(preparedTransfer))
-      const rpcForged = await Tezos.rpc.forgeOperations(toForge(preparedTransfer))
+      const forged = await forger.forge(toForge(preparedTransfer));
+      const rpcForged = await Tezos.rpc.forgeOperations(toForge(preparedTransfer));
 
-      expect(forged).toEqual(rpcForged)
+      expect(forged).toEqual(rpcForged);
 
       done();
     })
